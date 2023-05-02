3b4c07f5ae74dabc1e9f37e669e6a6a52bc74a24
oranjestrawberry <maartenrouws@gmail.com>
2023-05-01 21:58:54 -0300

    ficha

--- CodigosAqui2/Daniel.cpp
+++ CodigosAqui2/Daniel.cpp
@@ -1,6 +1,47 @@
 #include <iostream>
+#include <set>
+#include <iomanip>
+class Cadastro{
+public:
+    std::string nome;
+    unsigned int idade;
+    std::string rh;
+    double altura;
+};
+class Comparadora{
+public:
+bool operator()(const Cadastro &a, const Cadastro &b){
+    return a.nome < b.nome;
+}
+};
 
 int main(){
-    std::cout << "Ola daniel!" << std::endl;
-    return 0;
+    std::set<Cadastro, Comparadora> ficha;
+    Cadastro *aux = new Cadastro;
+    unsigned int ad = 1;
+    while(aux->nome != "s"){
+        std::cout << "pessoa n" << ad << std::endl;
+        std::cout << "-----------------------" << std::endl;
+        ad++;
+        getline(std::cin, aux->nome);
+        if(aux->nome == "s"){
+            break;
+        }
+        std::cin >> aux->idade;
+        std::cin.ignore();
+        getline(std::cin, aux->rh);
+        std::cin >> aux->altura;
+        std::cin.ignore();
+        ficha.insert(*aux);
+        aux = new Cadastro;
+     }
+
+    for(Cadastro a : ficha){
+    std::cout << std::setprecision(3);
+    std::cout << "Nome: " << a.nome << " "<< "Idade: " << a.idade << " "<< "rh: " << a.rh << " " << "Altura: ";
+    printf("%0.2lf\n",a.altura);
+    std::cout << "----------------------------------------------------------------------------------------------" << std::endl;
+
+    }
+    return 0; 
 }
